40ad30024265c3fb0e876bec692a4d4caf9fc25c
youssef <youssef.ahmed.talaat@icloud.com>
2026-02-11 10:18:28 +0200

    Fix User model for Filament production access

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Or add specific logic like: return $this->email === 'admin@namaq.com';
+    }
 
     /**
      * The attributes that are mass assignable.
