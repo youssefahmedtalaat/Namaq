916b5e6815945a6f0a71a6a46d719e1d8ac70c65
youssef <youssef.ahmed.talaat@icloud.com>
2026-02-12 17:36:43 +0200

    footer

--- app/Filament/Resources/FooterLinkResource/Pages/ListFooterLinks.php
+++ app/Filament/Resources/FooterLinkResource/Pages/ListFooterLinks.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\FooterLinkResource\Pages;
 
 use App\Filament\Resources\FooterLinkResource;
+use App\Models\GeneralSetting;
 use Filament\Actions;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\ListRecords;
 
 class ListFooterLinks extends ListRecords
@@ -13,7 +16,50 @@ class ListFooterLinks extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\Action::make('editAbout')
+                ->label('Edit About Us')
+                ->icon('heroicon-o-pencil-square')
+                ->fillForm(fn () => GeneralSetting::first()?->toArray() ?? [])
+                ->form([
+                    TextInput::make('footer_about_title')
+                        ->label('Title')
+                        ->required(),
+                    RichEditor::make('footer_about_description')
+                        ->label('Description')
+                        ->required(),
+                ])
+                ->action(function (array $data) {
+                    $settings = GeneralSetting::firstOrNew(['id' => 1]);
+                    $settings->fill($data);
+                    $settings->save();
+                }),
+
+            Actions\Action::make('editContact')
+                ->label('Edit Contact Us')
+                ->icon('heroicon-o-phone')
+                ->fillForm(fn () => GeneralSetting::first()?->toArray() ?? [])
+                ->form([
+                    TextInput::make('footer_contact_address')
+                        ->label('Address'),
+                    TextInput::make('footer_contact_phone')
+                        ->label('Phone'),
+                    TextInput::make('footer_contact_fax')
+                        ->label('Fax'),
+                    TextInput::make('footer_contact_email')
+                        ->label('Email')
+                        ->email(),
+                    TextInput::make('footer_contact_website')
+                        ->label('Website')
+                        ->url(),
+                ])
+                ->action(function (array $data) {
+                    $settings = GeneralSetting::firstOrNew(['id' => 1]);
+                    $settings->fill($data);
+                    $settings->save();
+                }),
+
+            Actions\CreateAction::make()
+                ->label('Add Link'),
         ];
     }
 }
