9488f351cfaf4d5f3583484c5df459c8d29864b6
youssef <youssef.ahmed.talaat@icloud.com>
2026-02-15 01:01:56 +0200

    same level view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,7 +89,7 @@
 
                     <div class="tp-caption font-weight-bold text-white text-center px-4" 
                         data-x="center" 
-                        data-y="['350','350','350','350']" 
+                        data-y="['350','350','350','390']" 
                         data-width="['auto','auto','auto','300']" 
                         data-height="none" 
                         data-whitespace="normal"
@@ -106,7 +106,7 @@
 
                     <div class="tp-caption sft text-center px-4" 
                         data-x="center" 
-                        data-y="['430','430','430','430']" 
+                        data-y="['430','430','430','470']" 
                         data-width="['auto','auto','auto','300']" 
                         data-height="none" 
                         data-whitespace="normal"
